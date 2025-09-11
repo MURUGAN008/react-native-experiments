9635075d044bfb991e1fd15107cec70a45614c3a
MURUGAN008 <murs4002@gmail.com>
2025-09-11 00:15:57 -0700

    add music image

--- MusicPlayer/pages/Dashboard.js
+++ MusicPlayer/pages/Dashboard.js
@@ -1,9 +1,18 @@
-import {View, Text} from "react-native"
+import {View, Text,Image} from "react-native"
+import {useState,useEffect} from "react"
+import axios from "axios"
 const Dashboard = () =>{
+    // const [isLoaded,setIsLoaded]=useState(false);
+    // useEffect(async()=>{
+    //     const image=await axios.get();
+    //     setIsLoaded("true");
+    // },[])
     return(
         <View>
             <Text>Enjoy the Music!</Text>
+            <Image source={{uri: "https://raw.githubusercontent.com/MURUGAN008/Music-Files/one-love-song/one-love.jpg"}} style={{width:200, height:200}}/>
         </View>
     )
 }
 export default Dashboard;
+//https://raw.githubusercontent.com/MURUGAN008/Music-Files/one-love-song/one-love.jpg
